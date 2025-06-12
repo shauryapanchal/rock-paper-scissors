0be494e141174b7f056c72d42ca66549fab1c990
Shaurya Panchal <122795698+shauryapanchal@users.noreply.github.com>
2025-06-12 11:51:50 +0000

    Add ui elements to the interface
    -Buttons for choice selection.
    -<p> logs for displaying the score and win/lose text.
    -alert message for displaying if user won or lost when one side reaches a score of 5
    -clearing the logs after conclusion.

--- javascript.js
+++ javascript.js
@@ -1,49 +1,52 @@
-function playGame(){
-    let humanScore=0;
-    let computerScore=0;
-    
-    for(let i=0; i<5; i++){
-        const humanChoice=getHumanChoice();
-        const computerChoice=getComputerChoice();
-        playRound(humanChoice, computerChoice);
-        function playRound(humanChoice, computerChoice){
-            if(humanChoice===computerChoice){
-                console.log("It's a tie");
-                return;
-            }
-            if(humanChoice==='rock'){
-                if(computerChoice==='paper'){
-                    console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
-                    computerScore++;
-                }
-                else{
-                    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-                    humanScore++;
-                }
-            }
-            else if(humanChoice==='paper'){
-                if(computerChoice==='scissors'){
-                    console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
-                    computerScore++;
-                }
-                else{
-                    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-                    humanScore++;
-                }
+let humanScore=0;
+let computerScore=0;
+function playGame(humanChoice){
+    const computerChoice=getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    function playRound(humanChoice, computerChoice){
+        const winText = document.createElement("p");
+        winText.textContent=`You Win! ${humanChoice} beats ${computerChoice}`;
+        const loseText = document.createElement("p");
+        loseText.textContent=`You Lose, ${computerChoice} beats ${humanChoice}`;
+        
+        const tieText = document.createElement("p");
+        tieText.textContent = `That's a tie`;
+        if(humanChoice===computerChoice){
+            choiceSelect.appendChild(tieText);
+            return;
+        }
+        if(humanChoice==='rock'){
+            if(computerChoice==='paper'){
+                choiceSelect.appendChild(loseText);
+                computerScore++;
             }
             else{
-                if(computerChoice==='paper'){
-                    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-                    computerScore++;
-                }
-                else{
-                    console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
-                    humanScore++;
-                }
+                choiceSelect.appendChild(winText);
+                humanScore++;
             }
         }
-        console.log(`You: ${humanScore}, Computer: ${computerScore}`);
+        else if(humanChoice==='paper'){
+            if(computerChoice==='scissors'){
+                choiceSelect.appendChild(loseText);
+                computerScore++;
+            }
+            else{
+                choiceSelect.appendChild(winText);
+                humanScore++;
+            }
+        }
+        else{
+            if(computerChoice==='paper'){
+                choiceSelect.appendChild(winText);
+                humanScore++;
+            }
+            else{
+                choiceSelect.appendChild(loseText);
+                computerScore++;
+            }
+        }
     }
+    console.log(`You: ${humanScore}, Computer: ${computerScore}`);
 }
 
 function getComputerChoice(){
@@ -63,3 +66,40 @@ function getHumanChoice(){
     let humanChoice = prompt("Choose one: Rock, Paper or Scissors")
     return humanChoice.toLowerCase();
 }
+
+function clear(){
+    const allLogs= choiceSelect.querySelectorAll("p");
+    allLogs.forEach(element => {
+        element.remove();
+    });
+}
+
+function handleClick(choice){
+    playGame(choice);
+    para.textContent=`You: ${humanScore}, Computer: ${computerScore}`;
+    choiceSelect.appendChild(para);
+    if(humanScore===5){
+        alert(`You won the Game`);
+        humanScore=0;
+        computerScore=0;
+        clear();
+    }
+    if(computerScore===5){
+        alert(`You lost the Game`);
+        humanScore=0;
+        computerScore=0;
+        clear();
+    }
+}
+
+const choiceSelect= document.querySelector(".choices")
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const para = document.createElement("p");
+const result = document.createElement("h2");
+
+
+rock.addEventListener('click',()=> handleClick('rock'));
+paper.addEventListener('click',()=> handleClick('paper'));
+scissors.addEventListener('click',()=> handleClick('scissors'));
