a0324697e9e22e219d84ec395ca8734284666268
Shaurya Panchal <122795698+shauryapanchal@users.noreply.github.com>
2025-06-12 07:17:53 +0000

    Add a tie condition to playRound() function
    in case the player and the computer choose same thing.
    display a tie message.

--- javascript.js
+++ javascript.js
@@ -7,6 +7,10 @@ const computerChoice=getComputerChoice();
 playRound(humanChoice, computerChoice);
 
 function playRound(humanChoice, computerChoice){
+    if(humanChoice===computerChoice){
+        console.log("It's a tie");
+        return;
+    }
     if(humanChoice==='rock'){
         if(computerChoice==='paper'){
             console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
